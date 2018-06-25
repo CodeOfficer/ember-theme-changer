f18365f0f0b8ef4d3dbeecba2f0295815aa84686
Dhiraj Indurthy <dhirajindurthy@gmail.com>
2018-06-25 11:45:51 -0500

    Update to asset-map service to allow choosing path type for assets
    New property isRelativePath added to allow user to choose the path type to be relative('/assets/theme-xyz') or non-relative(absolute- 'assets/theme-xyz'). 
    Property set to 'true' by default as its the mostly used way of accessing the asset theme files

--- addon/services/asset-map.js
+++ addon/services/asset-map.js
@@ -1,12 +1,26 @@
 import Service from '@ember/service';
 import { debug, warn } from '@ember/debug';
 import { isNone } from '@ember/utils';
-
+import { getOwner } from '@ember/application';
 
 export default Service.extend({
   enabled: false,
   assetMapHash: null,
+  prepend: '/',
+  isRelativePath: true,
+  
+  // @private
+  init() {
+    this._super(...arguments);
 
+    const owner = getOwner(this);
+    const ENV = owner.factoryFor('config:environment').class;
+    const config = ENV.theme;
+
+    if (!config.isRelativePath) {
+      this.set('isRelativePath', false);
+    }
+  },
   resolve(name) {
     const map = this.get('assetMapHash');
     let resolvedName;
@@ -26,7 +40,11 @@ export default Service.extend({
     } else {
       resolvedName = name;
     }
-
+    
+    if (this.get('isRelativePath')) {
+      return this.get('prepend') + resolvedName;
+    }
+    
     return resolvedName;
   }
 });
