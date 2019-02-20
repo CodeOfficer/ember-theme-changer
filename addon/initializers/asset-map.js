84cec2144836a3e2d0adf9297fe3c5b9edc01359
Jonathan Haines <jonno.haines@gmail.com>
2019-02-20 12:08:56 +1100

    switchout ember-ajax for ember-fetch

--- addon/initializers/asset-map.js
+++ addon/initializers/asset-map.js
@@ -1,4 +1,4 @@
-import request from 'ember-ajax/request';
+import fetch from 'fetch';
 import { isNone } from '@ember/utils';
 import AssetMap from 'ember-theme-changerr/services/asset-map';
 import { warn } from '@ember/debug';
@@ -24,8 +24,15 @@ export function initialize(app) {
     return;
   }
 
-  request('/assets/assetMap.json', {
-    dataType: 'json'
+  fetch('/assets/assetMap.json', {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Network response was not ok.');
   }).then((map) => {
     const prepend = config.theme.assetPrepend || map.prepend;
 
@@ -41,6 +48,7 @@ export function initialize(app) {
       'Error loading assetMap.json. Did you forget to set:\nfingerprint: {\n fingerprintAssetMap=true\n}\nin your ember-cli-build file?',
       { id: 'ember-theme-changerr.asset-map' }
     );
+    app.register('service:asset-map', AssetMap);
   }).finally(() => {
     app.advanceReadiness();
   });
--- tests/unit/initializers/asset-map-test.js
+++ tests/unit/initializers/asset-map-test.js
@@ -5,6 +5,24 @@ import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import { run } from '@ember/runloop';
 
+function assetMapSuccess() {
+  this.get('/assets/assetMap.json', () => {
+    return [200, { 'Content-Type': 'application/json' }, JSON.stringify({
+      prepend: '/',
+      assets: {
+        light: 'light.css',
+        dark: 'dark.css'
+      }
+    })];
+  });
+}
+
+function assetMapFailure() {
+  this.get('/assets/assetMap.json', () => {
+    return [404];
+  });
+}
+
 module('Unit | Initializer | asset-map', function(hooks) {
   setupTest(hooks);
 
@@ -16,17 +34,7 @@ module('Unit | Initializer | asset-map', function(hooks) {
     });
 
     this.application = this.TestApplication.create({ autoboot: false });
-    this.server = new Pretender(function() {
-      this.get('/assets/assetMap.json', () => {
-        return [200, { 'Content-Type': 'application/json' }, JSON.stringify({
-          prepend: '/',
-          assets: {
-            light: 'light.css',
-            dark: 'dark.css'
-          }
-        })];
-      });
-    });
+    this.server = new Pretender();
   });
 
   hooks.afterEach(function() {
@@ -41,6 +49,8 @@ module('Unit | Initializer | asset-map', function(hooks) {
       }
     });
 
+    this.server.map(assetMapSuccess);
+
     let instance;
 
     try {
@@ -51,6 +61,31 @@ module('Unit | Initializer | asset-map', function(hooks) {
 
       assert.equal(assetMap.resolve('light'), '/light.css');
       assert.equal(assetMap.resolve('dark'), '/dark.css');
+    } catch (error) {
+      assert.notOk(error);
+    } finally {
+      instance && run(instance, 'destroy');
+    }
+  });
+
+  test('it handles asset request failures', async function(assert) {
+    this.application.register('config:environment', {
+      theme: {
+        useAssetMap: true
+      }
+    });
+
+    this.server.map(assetMapFailure);
+
+    let instance;
+
+    try {
+      await this.application.boot();
+      instance = this.application.buildInstance();
+
+      const assetMap = instance.lookup('service:asset-map');
+
+      assert.equal(assetMap.enabled, false);
     } catch (error) {
       assert.notOk(error);
     } finally {
